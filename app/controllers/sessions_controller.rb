5573b0a96f531552addf42f03ba495c20daf2210
Garth Lilly <garff247@gmail.com>
2019-10-24 15:42:45 -0700

    added session controller with methods

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+
+    def new
+    end
+
+    def create
+
+    end
+
+    def logout
+        session.clear
+        flash[:notice] = "See you soon!"
+        redirect_to root_path
+    end
+    
+end

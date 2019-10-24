4337cc050810f88d93032144f700a314b70762b3
Garth Lilly <garff247@gmail.com>
2019-10-24 15:55:17 -0700

    remeved frequency from schedule

--- db/migrate/20191024222758_create_schedules.rb
+++ db/migrate/20191024222758_create_schedules.rb
@@ -3,7 +3,6 @@ class CreateSchedules < ActiveRecord::Migration[5.2]
     create_table :schedules do |t|
       t.integer :maid_id
       t.integer :client_id
-      t.string :frequency
       t.string :day_of_week
       t.integer :length_of_time
 

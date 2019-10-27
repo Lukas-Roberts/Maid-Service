17203577bd606b00c584657b9d973a1ec90ce774
Garth Lilly <garff247@gmail.com>
2019-10-27 14:57:52 -0700

    created 'create' method for accounts

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -5,7 +5,22 @@ class AccountsController < ApplicationController
     end
 
     def create
-    
+        if params[:password] == params[:password_confirmation] && params[:accountable_type]
+            @account = Account.create(account_params)
+            if @account.accountable_type == "maid"
+                redirect_to new_maid_path
+            else
+                redirect_to new_client_path
+            end
+        else
+            redirect_to login_path
+        end
+    end
+
+    private
+
+    def account_params
+        params.require(:account).permit(:username, :email, :password, :password_confirmation, :accountable_type)
     end
 
 end

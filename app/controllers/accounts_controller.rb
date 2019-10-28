4cbc287ebc31c59c2e5e5659e1f476325f57bd8f
Garth Lilly <garff247@gmail.com>
2019-10-27 18:04:24 -0700

    Creating maid 'create' method

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -5,10 +5,10 @@ class AccountsController < ApplicationController
     end
 
     def create
-        if params[:password] == params[:password_confirmation]
-            @account = Account.create(account_params)
+        @account = Account.new(account_params)
+        if @account.save
             session[:account_id] = @account.id
-            if @account.accountable_type == "maid"
+            if params[:accountable_type] == "maid"
                 redirect_to new_maid_path
             else
                 redirect_to new_client_path
@@ -21,7 +21,7 @@ class AccountsController < ApplicationController
     private
 
     def account_params
-        params.require(:account).permit(:username, :email, :password, :password_confirmation, :accountable_type)
+        params.require(:account).permit(:username, :email, :password, :password_confirmation)
     end
 
 end
--- app/controllers/maids_controller.rb
+++ app/controllers/maids_controller.rb
@@ -5,7 +5,18 @@ class MaidsController < ApplicationController
     end
 
     def create
-    
+        @maid = Maid.new(maid_params)
+        @account = Account.find(session[:account_id])
+        @maid.account = @account
+        if @maid.save
+            
+            
     end
     
+    private 
+    
+    def maid_params
+        params.require(:maid).permit(:first_name, :last_name, :experience)
+    end
+
 end

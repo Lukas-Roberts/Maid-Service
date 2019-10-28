0301be6383f6d30bc950bc2c1df8f228a3b297ad
Garth Lilly <garff247@gmail.com>
2019-10-28 13:35:04 -0700

    finished client 'create' method & strong params'

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,3 +1,24 @@
 class ClientsController < ApplicationController
 
+    def new
+        @client = Client.new
+    end
+
+    def create
+        @client = Client.new(client_params)
+        @account = Account.find(session[:account_id])
+        @account.accountable = @client
+        if @account.save
+            redirect_to client_path(@client)
+        else
+            redirect_to new_client_path
+        end
+    end
+
+    private
+
+    def client_params
+        params.require(:client).permit(:first_name, :last_name)
+    end
+    
 end

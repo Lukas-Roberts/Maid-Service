3ca6ae1196128ef2963612f86688ff7c9241e4cf
Garth Lilly <garff247@gmail.com>
2019-10-28 22:56:49 -0700

    created strong params for residences controller

--- app/controllers/residences_controller.rb
+++ app/controllers/residences_controller.rb
@@ -1,2 +1,29 @@
 class ResidencesController < ApplicationController
+
+    def new
+
+    end
+    
+    def create
+
+    end
+
+    def show
+
+    end
+
+    def edit
+
+    end
+
+    def update
+        
+    end
+
+    private
+
+    def residence_params
+        params.require(:residence).permit(:address, :residence_type, :city, :state, :number_of_bedrooms, :number_of_bathrooms)
+    end
+    
 end

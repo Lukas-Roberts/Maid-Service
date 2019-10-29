a71eca3f99aeb2f7a9f923ba9caf66d21a045538
Garth Lilly <garff247@gmail.com>
2019-10-28 18:12:47 -0700

    added before action to clients

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,5 +1,5 @@
 class ClientsController < ApplicationController
-
+    before_action :get_client, only: [:show, :edit, :update]
     def new
         @client = Client.new
     end
@@ -16,14 +16,24 @@ class ClientsController < ApplicationController
     end
 
     def show
-        @client = Client.find(params[:id])
         @residences = Residence.where(["client_id = :client_id", {client_id: params[:id]}])
+    end
+
+    def edit
+    end
+
+    def update
+
     end
 
     private
 
     def client_params
         params.require(:client).permit(:first_name, :last_name, :city, :state)
+    end
+
+    def get_client
+        @client = Client.find(params[:id])
     end
     
 end

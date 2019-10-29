995072c764ac627becb01846e5a91b1a983dfc52
Garth Lilly <garff247@gmail.com>
2019-10-29 00:13:14 -0700

    created strong params for schedules

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,3 +1,29 @@
 class SchedulesController < ApplicationController
 
+    def new
+
+    end
+
+    def create
+
+    end
+
+    def show
+
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    private
+
+    def schedule_params
+        params.require(:schedule).permit(:day_of_week, :length_of_time, :maid_id)
+    end
+    
 end
